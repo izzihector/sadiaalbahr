2fb0aaf19be0339f4bb0c878ce2feb6e60940e4b
Mohamed Shorbagi <shorbagi88@gmail.com>
2022-01-18 14:02:31 +0400

    sale report

--- gs_sale_report/__manifest__.py
+++ gs_sale_report/__manifest__.py
@@ -16,16 +16,16 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '15.0.0.0.3',
+    'category': 'Sales',
+    'version': '15.0.0.0.4',
 
     # any module necessary for this one to work correctly
     'depends': ['base','sale','gs_partner_customization'],
     'data': [
-        'views/company_arabic_address.xml',
+        # 'views/company_arabic_address.xml',
         'report/sale_order_paper_format.xml',
         # 'report/custom_header_footer.xml',
-        'report/sale_order_qutation_report.xml',
+        'report/sale_order_quotation_report.xml',
         'report/report_style.xml',
     ],
     "license": "Other proprietary",
