1650b2e6d3df86d93f77707e340e9431899224d9
Mohamed Shorbagi <shorbagi88@gmail.com>
2022-01-18 13:49:21 +0400

    sale

--- gs_sale_report/__manifest__.py
+++ gs_sale_report/__manifest__.py
@@ -17,14 +17,14 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '15.0.0.0.2',
+    'version': '15.0.0.0.3',
 
     # any module necessary for this one to work correctly
     'depends': ['base','sale','gs_partner_customization'],
     'data': [
         'views/company_arabic_address.xml',
         'report/sale_order_paper_format.xml',
-        'report/custom_header_footer.xml',
+        # 'report/custom_header_footer.xml',
         'report/sale_order_qutation_report.xml',
         'report/report_style.xml',
     ],
