75c67cb2608864a18ca5388ee8a9222f49825c59
M.Wasefy <mohammed.alwasefy@gmail.com>
2022-02-17 11:37:24 +0200

    [ADD] posted by / created by

--- journal_entry_report/models/models.py
+++ journal_entry_report/models/models.py
@@ -9,6 +9,13 @@ class JournalItem(models.Model):
 class JournalEntry(models.Model):
     _inherit = 'account.move'
 
+    posted_user = fields.Many2one('res.users')
+
+    def _post(self, soft=True):
+        self.posted_user = self.env.user.id
+        return super()._post(soft)
+
+
     def compute_amount_in_word_ar(self, amount):
         # num_word = num2words(amount, to='currency', lang='ar')
         # num_word = str(num_word) + ' فقط'
